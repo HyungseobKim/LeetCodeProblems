1f9cd7754b9b7a41d4b0fa3916ddfd9fb96b0aa0
HyungseobKim <69501583+HyungseobKim@users.noreply.github.com>
2024-03-16 10:34:37 +0900

    2024 ver. almost same

--- 1_TwoSum.cpp
+++ 1_TwoSum.cpp
@@ -2,24 +2,30 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         std::vector<int> result(2);
+
         std::unordered_map<int, int> table;
+        table[nums[0]] = 0;
+
         const int size = static_cast<int>(nums.size());
-        
-        for (int i = 0; i < size; ++i)
+
+        for (int i = 1; i < size; ++i)
         {
-            const int curr = nums[i];
-            std::unordered_map<int, int>::const_iterator complement = table.find(target - curr);
-            
+            const int num = nums[i];
+            std::unordered_map<int, int>::const_iterator complement = table.find(target - num);
+
             if (complement == table.end())
-                table[curr] = i;
+            {
+                table[num] = i;
+            }
             else
             {
-                result[0] = i;
-                result[1] = complement->second;
+                result[0] = complement->second;
+                result[1] = i;
+
                 return result;
             }
         }
-        
+
         return result;
     }
 };
