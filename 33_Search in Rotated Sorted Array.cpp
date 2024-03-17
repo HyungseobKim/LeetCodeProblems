fd5605c638aac6db0704eb1fdc505bb2aafc56c7
HyungseobKim <69501583+HyungseobKim@users.noreply.github.com>
2024-03-17 16:45:20 +0900

    Update 33_Search in Rotated Sorted Array.cpp
    adding an iterative version, 100%

--- 33_Search in Rotated Sorted Array.cpp
+++ 33_Search in Rotated Sorted Array.cpp
@@ -68,3 +68,100 @@ private:
 private:
     int m_target;
 };
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int min = 0;
+        int max = nums.size() - 1;
+
+        while (min <= max)
+        {
+            const int mid = (min + max) / 2;
+            const int num_mid = nums[mid];
+
+            if (num_mid == target)
+            {
+                return mid;
+            }
+
+            if (min == max)
+            {
+                return -1;
+            }
+
+            const int num_min = nums[min];
+            const int num_max = nums[max];
+
+            if (num_mid > num_max)
+            {
+                if (target < num_mid)
+                {
+                    if (target == num_min)
+                    {
+                        return min;
+                    }
+
+                    if (target < num_min)
+                    {
+                        if (min == mid)
+                        {
+                            ++min;
+                        }
+                        else
+                        {
+                            min = mid;
+                        }
+                    }
+                    else
+                    {
+                        max = mid;
+                    }
+                }
+                else
+                {
+                    if (min == mid)
+                    {
+                        ++min;
+                    }
+                    else
+                    {
+                        min = mid;
+                    }
+                }
+            }
+            else
+            {
+                if (target < num_mid)
+                {
+                    max = mid;
+                }
+                else
+                {
+                    if (target == num_max)
+                    {
+                        return max;
+                    }
+
+                    if (target < num_max)
+                    {
+                        if (min == mid)
+                        {
+                            ++min;
+                        }
+                        else
+                        {
+                            min = mid;
+                        }
+                    }
+                    else
+                    {
+                        max = mid;
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+};
