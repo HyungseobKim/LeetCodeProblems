6e9b4aa1f7176d4cd80dab8498d861f9cf23fc80
HyungseobKim <69501583+HyungseobKim@users.noreply.github.com>
2024-03-18 20:28:14 +0900

    Create 232_Implement Queue using Stacks.cpp
    100% but not sure if it takes O{1) time complexity.

---
+++ 232_Implement Queue using Stacks.cpp
@@ -0,0 +1,69 @@
+/*
+	Follow-up: Can you implement the queue such that each operation is amortized O(1) time complexity? In other words, performing n operations will take overall O(n) time even if one of those operations may take longer.
+*/
+class MyQueue {
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if (outStack.empty() == false)
+        {
+            MoveAll(outStack, inStack);
+        }
+
+        inStack.push(x);
+    }
+    
+    int pop() {
+        if (outStack.empty())
+        {
+            MoveAll(inStack, outStack);
+        }
+
+        const int top = outStack.top();
+        outStack.pop();
+
+        return top;
+    }
+    
+    int peek() {
+        if (outStack.empty())
+        {
+            MoveAll(inStack, outStack);
+        }
+
+        return outStack.top();
+    }
+    
+    bool empty() {
+        return inStack.empty() && outStack.empty();
+    }
+
+private:
+    void MoveAll(std::stack<int>& stackToClear, std::stack<int>& stackToFill)
+    {
+        const int size = stackToClear.size();
+
+        for (int i = 0; i < size; ++i)
+        {
+            const int top = stackToClear.top();
+            stackToClear.pop();
+
+            stackToFill.push(top);
+        }
+    }
+
+    std::stack<int> inStack;
+    std::stack<int> outStack;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
