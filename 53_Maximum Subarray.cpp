d8a8cba79b868eaad8d201a912900bf76866c4a1
HyungseobKim <69501583+HyungseobKim@users.noreply.github.com>
2024-08-28 12:08:11 +0900

    Update 53_Maximum Subarray.cpp
    adding another solution which is probably worse than existing one

--- 53_Maximum Subarray.cpp
+++ 53_Maximum Subarray.cpp
@@ -60,4 +60,77 @@ public:
         
         return best;
     }
+	
+	int maxSubArray(vector<int>& nums) {
+        const int size = nums.size();
+        int index = 0;
+
+        if (nums[0] < 0)
+        {
+            int biggest_negative = nums[0];
+            int negative_index = 1;
+
+            while (negative_index < size)
+            {
+                const int num = nums[negative_index];
+                if (num < 0)
+                {
+                    biggest_negative = std::max(biggest_negative, num);
+                    ++negative_index;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (negative_index == size)
+            {
+                return biggest_negative;
+            }
+
+            index = negative_index;
+        }
+        
+        int maxSum = nums[index];
+        int sum = maxSum;
+
+        for (int i = index + 1; i < size; ++i)
+        {
+            const int num = nums[i];
+
+            if (num >= 0)
+            {
+                sum += num;
+            }
+            else
+            {
+                maxSum = std::max(maxSum, sum);
+                
+                int negative_index = i;
+                while (negative_index < size)
+                {
+                    if (nums[negative_index] <= 0)
+                    {
+                        sum += nums[negative_index];
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    ++negative_index;
+                }
+
+                if (sum < 0)
+                {
+                    sum = 0;
+                }
+
+                i = negative_index - 1;
+            }
+        }
+
+        return std::max(maxSum, sum);
+    }
 };
