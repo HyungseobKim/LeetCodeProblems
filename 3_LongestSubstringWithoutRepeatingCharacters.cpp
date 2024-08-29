40acfd64789677bf1672ad8e51cd5b3327140129
HyungseobKim <69501583+HyungseobKim@users.noreply.github.com>
2024-08-29 13:17:32 +0900

    Update 3_LongestSubstringWithoutRepeatingCharacters.cpp
    the same algorithm.

--- 3_LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         std::unordered_map<char, int> table;
+        const int length = s.length();
 
-        int subStringBegin = 0;
-        int length = 0;
+        int substring_begin = 0;
+        int substring_length = 0;
         int longest = 0;
 
-        const int end = s.length();
-
-        for (int i = 0; i < end; ++i)
+        for (int i = 0; i < length; ++i)
         {
             const char c = s[i];
+            const auto itr = table.find(c);
 
-            if (table.find(c) == table.end() || table[c] < subStringBegin)
+            if (itr != table.end() && itr->second >= substring_begin)
             {
-                ++length;
+                longest = std::max(longest, substring_length);
+
+                substring_length -= (itr->second - substring_begin);
+                substring_begin = itr->second + 1;
             }
             else
             {
-                longest = std::max(longest, length);
-
-                length -= (table[c] - subStringBegin);
-                subStringBegin = table[c] + 1;
+                ++substring_length;
             }
 
             table[c] = i;
         }
 
-        return std::max(longest, length);
+        return std::max(longest, substring_length);
     }
 };
