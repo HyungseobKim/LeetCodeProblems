9df23920fa835bcdf9c00d391c28f54f16ddf5ce
HyungseobKim <69501583+HyungseobKim@users.noreply.github.com>
2024-08-31 12:23:14 +0900

    Update 70_Climbing Stairs.cpp
    the same algorithm, but looks better

--- 70_Climbing Stairs.cpp
+++ 70_Climbing Stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n < 2)
+        if (n == 1)
         {
-            return n;
+            return 1;
         }
-        
-        table = std::vector<int>(n, -1);
-        table[0] = 1;
-        table[1] = 2;
+
+        table = std::vector<int>(n + 1, -1);
+        table[1] = 1;
+        table[2] = 2;
 
         return climbStairs_rec(n);
     }
@@ -16,15 +16,12 @@ public:
 private:
     int climbStairs_rec(int n)
     {
-        if (table[n-1] > 0)
+        if (table[n] == -1)
         {
-            return table[n-1];
+            table[n] = climbStairs_rec(n - 1) + climbStairs_rec(n - 2);
         }
-        
-        const int cases = climbStairs_rec(n - 1) + climbStairs_rec(n - 2);
-        table[n-1] = cases;
 
-        return cases;
+        return table[n];
     }
 
     std::vector<int> table;
