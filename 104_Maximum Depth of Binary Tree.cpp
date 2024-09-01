65e18f6824a07b8c3d44a7235c5ba43e1dc9c3f6
HyungseobKim <69501583+HyungseobKim@users.noreply.github.com>
2024-09-02 08:22:01 +0900

    Update 104_Maximum Depth of Binary Tree.cpp
    the simplest way

--- 104_Maximum Depth of Binary Tree.cpp
+++ 104_Maximum Depth of Binary Tree.cpp
@@ -12,79 +12,11 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        maxDepth_rec(root, 0);
-        
-        return best;
-    }
-    
-private:
-    void maxDepth_rec(TreeNode* node, int depth)
-    {
-        if (node == nullptr)
-        {
-            if (depth > best)
-            {
-                best = depth;
-            }
-            
-            return;
-        }
-        
-        ++depth;
-        
-        maxDepth_rec(node->left, depth);
-        maxDepth_rec(node->right, depth);
-    }
-    
-private:
-    int best = 0;
-};
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int maxDepth(TreeNode* root) {
         if (root == nullptr)
         {
             return 0;
         }
-        return maxDepth_rec(root, 0);
-    }
 
-private:
-    int maxDepth_rec(TreeNode* node, int depth)
-    {
-        ++depth;
-
-        if (node->left == nullptr && node->right == nullptr)
-        {
-            return depth;
-        }
-
-        int leftDepth = 0;
-
-        if (node->left)
-        {
-            leftDepth = maxDepth_rec(node->left, depth);
-        }
-
-        int rightDepth = 0;
-
-        if (node->right)
-        {
-            rightDepth = maxDepth_rec(node->right, depth);
-        }
-
-        return std::max(leftDepth, rightDepth);
+        return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
