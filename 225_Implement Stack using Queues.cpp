81994c09413af85cecc0162ca654d70a3e3e2b69
HyungseobKim <69501583+HyungseobKim@users.noreply.github.com>
2024-09-02 00:56:31 +0900

    Update 225_Implement Stack using Queues.cpp
    Adding a simpler approach: just storing the top element instead of having a boolean about if the top is at the front.

--- 225_Implement Stack using Queues.cpp
+++ 225_Implement Stack using Queues.cpp
@@ -65,3 +65,53 @@ private:
  * int param_3 = obj->top();
  * bool param_4 = obj->empty();
  */
+ 
+class MyStack {
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        topElement = x;
+    }
+    
+    int pop() {
+        const int numShift = q.size() - 2;
+        for (int i = 0; i < numShift; ++i)
+        {
+            const int element = q.front();
+            q.pop();
+            q.push(element);
+        }
+
+        topElement = q.front();
+        q.pop();
+        q.push(topElement);
+
+        const int element = q.front();
+        q.pop();
+        return element;
+    }
+    
+    int top() {
+        return topElement;
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+private:
+    std::queue<int> q;
+    int topElement;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
