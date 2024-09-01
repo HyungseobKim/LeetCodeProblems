4ded73f7133b1d831edbcfca8a0e5cbb5869ec20
HyungseobKim <69501583+HyungseobKim@users.noreply.github.com>
2024-09-02 08:29:33 +0900

    Update 226_Invert Binary Tree.cpp
    the simplest way

--- 226_Invert Binary Tree.cpp
+++ 226_Invert Binary Tree.cpp
@@ -12,62 +12,14 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if (root)
+        if (root != nullptr)
         {
-            invertTree_rec(root);
+            TreeNode* const left = invertTree(root->left);
+            root->left = invertTree(root->right);
+            root->right = left;
+			// Or can use std::swap
         }
-        
+
         return root;
-    }
-    
-private:
-    void invertTree_rec(TreeNode* node)
-    {
-        std::swap(node->left, node->right);
-        
-        if (node->left)
-        {
-            invertTree(node->left);
-        }
-        
-        if (node->right)
-        {
-            invertTree(node->right);
-        }
-    }
-};
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) {
-        invertTree_rec(root);
-        return root;
-    }
-
-private:
-    void invertTree_rec(TreeNode* node)
-    {
-        if (node == nullptr)
-        {
-            return;
-        }
-
-        TreeNode* temp = node->left;
-        node->left = node->right;
-        node->right = temp;
-
-        invertTree_rec(node->left);
-        invertTree_rec(node->right);
     }
 };
