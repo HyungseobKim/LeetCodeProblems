7ff34c27bf9004a260ac97033f0ae6567e3552cd
HyungseobKim <69501583+HyungseobKim@users.noreply.github.com>
2025-05-08 06:33:21 +0900

    Update 41_First Missing Positive.cpp
    adding the version that doesn't use any extra space

--- 41_First Missing Positive.cpp
+++ 41_First Missing Positive.cpp
@@ -1,6 +1,49 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        const int size = nums.size();
+
+        for (int i = 0; i < size; ++i)
+        {
+            if (nums[i] < 0)
+            {
+                nums[i] = 0;
+            }
+        }
+
+        for (int i = 0; i < size; ++i)
+        {
+            const int num = std::abs(nums[i]);
+            const int index = num - 1;
+
+            if (index >= 0 && index < size)
+            {
+                if (nums[index] == 0)
+                {
+                    nums[index] = -num;
+                }
+                else
+                {
+                    nums[index] = -std::abs(nums[index]);
+                }
+            }
+        }
+
+        for (int i = 0; i < size; ++i)
+        {
+            if (nums[i] >= 0)
+            {
+                return i + 1;
+            }
+        }
+
+        return size + 1;
+    }
+};
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
         const int size = static_cast<int>(nums.size());
         std::vector<bool> table(size, false);
         
