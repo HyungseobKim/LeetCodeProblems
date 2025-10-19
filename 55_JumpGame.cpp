81a502c5a84cd4a5a7b29fb31eb091f45dac6f6f
HyungseobKim <69501583+HyungseobKim@users.noreply.github.com>
2025-10-19 20:52:42 +0900

    Update 55_JumpGame.cpp

--- 55_JumpGame.cpp
+++ 55_JumpGame.cpp
@@ -2,35 +2,16 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         const int end = nums.size() - 1;
-        
-        int index = 0;
-        int prev = 0;
-        
-        while (index < end)
+        int reachable = 0;
+        for (int i = 0; i <= reachable; ++i)
         {
-            const int num = nums[index];
-            
-            if (num == 0) // Need to go back
+            const int jump = i + nums[i];
+            if (jump >= end)
             {
-                int max = 0;
-                
-                for (int i = prev; i < index; ++i)
-                {
-                    const int jump = i + nums[i];
-                    if (jump > max)
-                        max = jump;
-                }
-                
-                if (max <= index)
-                    return false;
-                
-                prev = index+1;
-                index = max;
+                return true;
             }
-            
-            index += num;
+            reachable = std::max(jump, reachable);
         }
-        
-        return true;
+        return false;
     }
 };
